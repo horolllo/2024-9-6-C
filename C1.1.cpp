dd0063f1485fe929c9d3d9183cacfb903deef951
horolllo <hyeonujo439@gmail.com>
2024-09-06 20:49:51 +0900

    Add files via upload

---
+++ C1.1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%%Solar Tracking System%%\n");
+	printf("\\Smart Devices\\\n");
+	printf("\"Matrix\"\n");
+	printf("\'Hello Robot'\n");
+	
+	
+	printf("\"%%\\학교가기 싫다\'\"");
+	 
+	return 0;
+}
+
