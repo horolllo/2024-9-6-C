67bd604950942013a93679ff00b1964d94870ea0
horolllo <hyeonujo439@gmail.com>
2024-09-13 20:52:32 +0900

    Create 0913.cpp

---
+++ 0913.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+  int s;
+  s = 3 * 4 / 2;
+  printf("%d", s);
+  
+  return 0;
+}
+
+/*
+#include <stdio.h>
+int main()
+{
+	printf("%d년 %d월 %s승리\n", 2019, 3, "알파고");
+	printf("%s프린터로 %s 짓기\n", "3D", "집");
+	printf("원주율 = %lf\n", 3.141592653589793238); 
+	printf("%.3lf 마라톤\n", 42.195);
+	
+	return 0;
+ } 
+*/
