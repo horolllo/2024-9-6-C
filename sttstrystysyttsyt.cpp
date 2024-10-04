1f4ac482768403e3faf137040d4fdf08220b384b
horolllo <hyeonujo439@gmail.com>
2024-10-04 19:55:26 +0900

    Add files via upload

---
+++ sttstrystysyttsyt.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int w, h;
+	double s;
+	
+	scanf("%d %d", &w, &h);
+	s = 1.0 * w * h / 2;//woooooooooooooooooooooooooooooooooooooooooooow!
+	
+	printf("%lf", s);
+	
+	
+	return 0;
+}
